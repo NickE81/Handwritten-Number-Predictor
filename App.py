2a28a397ab204a3c7834009d2080740a2cccc944
Nick Elliott <nick.elliott1081@gmail.com>
2025-11-24 19:27:37 -0500

    Adding letter prediction using trained model to app

--- App.py
+++ App.py
@@ -1,11 +1,20 @@
 from flask  import Flask, request, render_template
 from keras.saving import load_model
 from keras.utils import load_img
+from keras.preprocessing import image
+import numpy as np
 import base64
+from io import BytesIO
+
 
 app = Flask(__name__)
 
 model = load_model("model.keras")
+
+def make_prediction(img_array):
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis = 1)
+    return predicted_class[0]
 
 @app.route("/")
 def home():
@@ -16,9 +25,16 @@ def predict():
     img_file = request.files.get("input-file")
     if img_file is None:
         return render_template("predict.html", img = None)
-    img_files_as_bytes = img_file.read()
-    img = base64.b64encode(img_files_as_bytes).decode("ascii")
-    return render_template("predict.html", img = img)
+    img_as_bytes = img_file.read()
+    img = image.load_img(BytesIO(img_as_bytes), target_size = (28, 28), color_mode = "grayscale")
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis = 0)
+    img_array = img_array / 255
+
+    predicted_class = make_prediction(img_array)
+
+    img_decoded = base64.b64encode(img_as_bytes).decode("ascii")
+    return render_template("predict.html", img = img_decoded, predicted_class = predicted_class)
 
 if __name__ == "__main__":
     app.run(debug = True)
